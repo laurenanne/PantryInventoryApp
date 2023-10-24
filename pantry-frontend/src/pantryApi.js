6f049e6fc6f04e48c0d80997552987084172ffd5
lauren brady <lauren.a.brady@gmail.com>
2023-10-24 12:51:00 -0400

    testing

--- pantry-frontend/src/pantryApi.js
+++ pantry-frontend/src/pantryApi.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = process.env.REACT_APP_BASE_URL || "http://localhost:3001";
+const BASE_URL = "https://stp-pantry.onrender.com" || "http://localhost:3001";
 
 class PantryApi {
   static token;
