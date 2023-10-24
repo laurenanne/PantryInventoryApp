8d6fcb7d97329b1456f1d6e1222db3a5b637eb77
lauren brady <lauren.a.brady@gmail.com>
2023-10-24 12:33:01 -0400

    deploy

---
+++ pantry-frontend/build/precache-manifest.27359f6b419e7397ef7556d583b05bef.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f220d10834dfb58dcfa6",
+    "url": "/static/css/main.dadd5d68.chunk.css"
+  },
+  {
+    "revision": "f220d10834dfb58dcfa6",
+    "url": "/static/js/main.f220d108.chunk.js"
+  },
+  {
+    "revision": "189e54ec23ab070d4209",
+    "url": "/static/js/1.189e54ec.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "0572e8dd8d272e16aa6bf1eb1fc6c567",
+    "url": "/index.html"
+  }
+];
