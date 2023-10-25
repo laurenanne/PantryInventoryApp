17924893acce4a50dad2c5957a54d91162e2d754
lauren brady <lauren.a.brady@gmail.com>
2023-10-25 10:08:47 -0400

    config

--- pantry-backend/config.js
+++ pantry-backend/config.js
@@ -16,7 +16,7 @@ function getDatabaseUri() {
 const BCRYPT_WORK_FACTOR = process.env.NODE_ENV === "test" ? 1 : 12;
 
 console.log("Pantry App Config:".green);
-
+console.log(dns.lookup(host));
 console.log("PORT:".yellow, PORT.toString());
 console.log("BCRYPT_WORK_FACTOR".yellow, BCRYPT_WORK_FACTOR);
 console.log("Database:".yellow, getDatabaseUri());
