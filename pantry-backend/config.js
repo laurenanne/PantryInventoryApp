6c26b28d96aa6f3ce59ac0fd17dfd5439ac1ffc0
lauren brady <lauren.a.brady@gmail.com>
2023-10-24 23:31:48 -0400

    update pg

--- pantry-backend/config.js
+++ pantry-backend/config.js
@@ -15,12 +15,12 @@ function getDatabaseUri() {
 
 const BCRYPT_WORK_FACTOR = process.env.NODE_ENV === "test" ? 1 : 12;
 
-console.log("Pantry App Config:".green);
-console.log("SECRET_KEY:".yellow, SECRET_KEY);
-console.log("PORT:".yellow, PORT.toString());
-console.log("BCRYPT_WORK_FACTOR".yellow, BCRYPT_WORK_FACTOR);
-console.log("Database:".yellow, getDatabaseUri());
-console.log("---");
+// console.log("Pantry App Config:".green);
+// console.log("SECRET_KEY:".yellow, SECRET_KEY);
+// console.log("PORT:".yellow, PORT.toString());
+// console.log("BCRYPT_WORK_FACTOR".yellow, BCRYPT_WORK_FACTOR);
+// console.log("Database:".yellow, getDatabaseUri());
+// console.log("---");
 
 module.exports = {
   SECRET_KEY,
