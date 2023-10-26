fd7756cf91ec484e793d5c2a15feaeeed0650b40
lauren brady <lauren.a.brady@gmail.com>
2023-10-26 08:42:32 -0400

    cleanup

--- pantry-frontend/src/pantryApi.js
+++ pantry-frontend/src/pantryApi.js
@@ -25,9 +25,7 @@ class PantryApi {
 
   // validate a user sign In, returns a token
   static async login(data) {
-    console.log(data);
     let res = await this.request("users/login", data, "post");
-    console.log(res);
     return res.token;
   }
 
